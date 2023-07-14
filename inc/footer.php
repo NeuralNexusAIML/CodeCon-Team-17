e53a7076e118b4c73bfadeb47a053b74047c8da6
PallaviReddyV <vavelalapallavi@gmail.com>
2023-07-14 07:20:29 +0530

    done

---
+++ inc/footer.php
@@ -0,0 +1,24 @@
+
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+        <h3 class="h-font fw-bold fs-3 mb-2">KNOW_CBIT</h3>
+        <p>
+            Hey folks! this website is to provide a user friendly information portal which can take you to all the answers you are searching for CBIT!
+        </p>
+        </div>
+        <div class="col-lg-4 p-4">
+        <h5 class=""mb-3>Follow us</h5>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+            <i class="bi bi-twitter"></i> twitter</a><br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+            <i class="bi bi-facebook"></i> Facebook</a><br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none ">
+            <i class="bi bi-instagram"></i> instagram</a><br>       
+        </div>
+    </div>
+</div>
+
+<h6 class="text-center bg-dark text-white p-3 m-0">designed and devloped by MSPR FORCE</h6>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
---
+++ inc/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light bg-white px-lg-2 py-lg-2 shadow-sm sticky-top">
+    <div class="container-fluid">
+    <a class="navbar-brand me-5 fw-bold fs-1 h-font " align="center" href="index.php"> KNOW_CHAITANYA BHARATHI INSTITUTE OF TECHNOLOGY
+    </a>
+    <button class="navbar-toggler shadow-non" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    
+</div>
+</nav>
